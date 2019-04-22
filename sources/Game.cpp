7f3dc9363fa4602609f82508363629e589c8007d
Sireanu Roland <roland@localhost.localdomain>
2019-04-22 19:21:26 +0300

    .gitignore is working now !

--- sources/Game.cpp
+++ sources/Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include "SFML/Window/Joystick.hpp"
 
 void Game::Render()
 {
@@ -35,28 +36,44 @@ void Game::HandleInput()
 {
 
 	sf::Event event;
-
+	bool joystickConnected = sf::Joystick::isConnected(0);
 
 	while(m_window.GetRenderWindow().pollEvent(event))
 	{		
-		
-		if(event.type == sf::Event::KeyPressed)
+		if(joystickConnected)
 		{
-			if(event.key.code == sf::Keyboard::Left)
+			if(event.type == sf::Event::JoystickButtonPressed)
 			{
-				m_hungryDot.SetDirection(HungryDot::Direction::LEFT);
+				if(sf::Joystick::isButtonPressed(0 , 1))
+					m_hungryDot.SetDirection(HungryDot::Direction::RIGHT);
+				if(sf::Joystick::isButtonPressed(0 , 2))
+					m_hungryDot.SetDirection(HungryDot::Direction::UP);	
+				if(sf::Joystick::isButtonPressed(0 , 3))
+					m_hungryDot.SetDirection(HungryDot::Direction::LEFT);
+				if(sf::Joystick::isButtonPressed(0 , 0))
+					m_hungryDot.SetDirection(HungryDot::Direction::DOWN);	
 			}
-			else if(event.key.code == sf::Keyboard::Right)
+		}
+		else 
+		{
+			if(event.type == sf::Event::KeyPressed)
 			{
-				m_hungryDot.SetDirection(HungryDot::Direction::RIGHT);
-			}
-			else if(event.key.code == sf::Keyboard::Up)			
-			{
-				m_hungryDot.SetDirection(HungryDot::Direction::UP);	
-			}
-			else if(event.key.code == sf::Keyboard::Down)
-			{
-				m_hungryDot.SetDirection(HungryDot::Direction::DOWN);	
+				if(event.key.code == sf::Keyboard::Left)
+				{
+					m_hungryDot.SetDirection(HungryDot::Direction::LEFT);
+				}
+				else if(event.key.code == sf::Keyboard::Right)
+				{
+					m_hungryDot.SetDirection(HungryDot::Direction::RIGHT);
+				}
+				else if(event.key.code == sf::Keyboard::Up)			
+				{
+					m_hungryDot.SetDirection(HungryDot::Direction::UP);	
+				}
+				else if(event.key.code == sf::Keyboard::Down)
+				{
+					m_hungryDot.SetDirection(HungryDot::Direction::DOWN);	
+				}
 			}
 		}
 		
